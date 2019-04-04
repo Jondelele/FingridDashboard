39619a29ef75c7e546fd2203728be4dd089be05a
Joona Prehti <joona.prehti@tuni.fi>
2019-04-04 20:59:38 +0300

    x-api-key moved to config file

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   httpPort: 8888,
-  secret: 'fdfbddfbfbdfdb43434242srg'
+  secret: 'fdfbddfbfbdfdb43434242srg',
+  xApiKey: 'MOwvzyd8yH9KbABqnpCIZ49Lzkw2ruay7Yc1dcT3'
 };
--- routes/api_fingrid.js
+++ routes/api_fingrid.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const request = require('request');
-var bodyParser = require('body-parser');
+const config = require('../config');
 
 router.get('/data', function (req, res) {
   request({
     url: 'https://api.fingrid.fi/v1/variable/58/events/json?start_time=2019-01-01T01%3A01%3A01Z&end_time=2019-03-01T01%3A01%3A01Z',
     json: true,
     headers: {
-      'x-api-key': 'MOwvzyd8yH9KbABqnpCIZ49Lzkw2ruay7Yc1dcT3'
+      'x-api-key': config.xApiKey
     }
   }, (error, response, body) => {
     res.json(body);
