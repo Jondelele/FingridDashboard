e4c6cf6d6002dba2847bddf3fe763634c206752a
Joona Prehti <joona.prehti@tuni.fi>
2019-04-12 13:15:15 +0300

    Disabled live-reload

--- app.js
+++ app.js
@@ -11,36 +11,36 @@ var express = require('express');
 
 var app = express();
 
-var livereload = require('easy-livereload');
-var file_type_map = {
-  jade: 'html', // `index.jade` maps to `index.html`
-  html: 'html',
-  js: 'js',
-  styl: 'css', // `styles/site.styl` maps to `styles/site.css`
-  scss: 'css', // `styles/site.scss` maps to `styles/site.css`
-  sass: 'css', // `styles/site.scss` maps to `styles/site.css`
-  less: 'css' // `styles/site.scss` maps to `styles/site.css`
-  // add the file type being edited and what you want it to be mapped to.
-};
+// var livereload = require('easy-livereload');
+// var file_type_map = {
+//   jade: 'html', // `index.jade` maps to `index.html`
+//   html: 'html',
+//   js: 'js',
+//   styl: 'css', // `styles/site.styl` maps to `styles/site.css`
+//   scss: 'css', // `styles/site.scss` maps to `styles/site.css`
+//   sass: 'css', // `styles/site.scss` maps to `styles/site.css`
+//   less: 'css' // `styles/site.scss` maps to `styles/site.css`
+//   // add the file type being edited and what you want it to be mapped to.
+// };
 
 // store the generated regex of the object keys
-var file_type_regex = new RegExp('\\.(' + Object.keys(file_type_map).join('|') + ')$');
+// var file_type_regex = new RegExp('\\.(' + Object.keys(file_type_map).join('|') + ')$');
 
-app.use(livereload({
-  watchDirs: [
-    path.join(__dirname, 'public'),
-  ],
-  checkFunc: function(file) {
-    return file_type_regex.test(file);
-  },
-  renameFunc: function(file) {
-    // remap extention of the file path to one of the extentions in `file_type_map`
-    return file.replace(file_type_regex, function(extention) {
-      return '.' + file_type_map[extention.slice(1)];
-    });
-  },
-  port: process.env.LIVERELOAD_PORT || 35729
-}));
+// app.use(livereload({
+//   watchDirs: [
+//     path.join(__dirname, 'public'),
+//   ],
+//   checkFunc: function(file) {
+//     return file_type_regex.test(file);
+//   },
+//   renameFunc: function(file) {
+//     // remap extention of the file path to one of the extentions in `file_type_map`
+//     return file.replace(file_type_regex, function(extention) {
+//       return '.' + file_type_map[extention.slice(1)];
+//     });
+//   },
+//   port: process.env.LIVERELOAD_PORT || 35729
+// }));
 
 
 // Use ottaa middlewaren käyttöön.
